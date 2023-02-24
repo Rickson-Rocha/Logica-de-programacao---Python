c191d94384ec90569bb026ce09af5990b2916b37
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-02-24 20:53:38 -0300

    resolução questão 01

---
+++ Listas/exercicio01.py
@@ -0,0 +1,6 @@
+#Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+numeros = []
+for i in range(5):
+    numero = int(input('Digite um número: '))
+    numeros.append(numero)
+print(numeros)
