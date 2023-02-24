b9cd80a2c6249a7466732c4b8a59238d273aca63
Rickson-Rocha <ricksonroccha21@gmail.com>
2023-02-24 19:29:51 -0300

    resolução questão 18

---
+++ Estrutura Repeticao/exercicio18.py
@@ -0,0 +1,14 @@
+#Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+
+qtdNumero = int(input('Quantos números deseja fornecer: '))
+soma = 0
+maiorNumero = 0
+menorNumero = 0
+for i in range(qtdNumero):
+    numero = int(input(f'{i+1}º número: '))
+    soma+= numero
+    if numero>maiorNumero:
+        maiorNumero = numero
+    elif menorNumero==0 or numero<menorNumero:
+        menorNumero =  numero
+print(f'Soma dos valores repassados: {soma}  | Maior número encontrado : {maiorNumero} | menor número encontrado {menorNumero}')
